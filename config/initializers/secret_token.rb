5dbdedf0bd42dfa919503902a215cd23393a077a
Andrew Cranston <andrew.cranston@gree.co.jp>
2013-08-13 14:57:08 -0700

    initial commit of rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyMove::Application.config.secret_key_base = 'd37f6a8bdcdcfb66ca8b1ff35c43436855519fb3b31a968bd03242b59e59d76c3e830716bff4e74169cccea47fa7dc4f541dd3adc494b4201adf645b1691f637'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyMove::Application.config.session_store :cookie_store, key: '_my_move_session'
